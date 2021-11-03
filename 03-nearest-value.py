1362c406064c1a30a9127ccceb9dacee16a48565
= <=>
2021-11-03 19:32:00 -0300

    Desafios hasta el 4

---
+++ 03-nearest-value.py
@@ -0,0 +1,24 @@
+def nearest_value(values: set, one: int) -> int:
+    res = max(values)
+    distance = abs(res-one)
+    for v in values:
+        if (abs(v-one) < distance) or ( abs(v-one) == distance and v < res):
+            res = v
+            distance = abs(v-one)
+    return res
+
+
+if __name__ == '__main__':
+    print("Example:")
+    # print(nearest_value({4, 7, 10, 11, 12, 17}, 9))
+    print(nearest_value([0,-2],-1))
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 9) == 10
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 8) == 7
+    assert nearest_value({4, 8, 10, 11, 12, 17}, 9) == 8
+    assert nearest_value({4, 9, 10, 11, 12, 17}, 9) == 9
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 0) == 4
+    assert nearest_value({4, 7, 10, 11, 12, 17}, 100) == 17
+    assert nearest_value({5, 10, 8, 12, 89, 100}, 7) == 8
+    assert nearest_value({-1, 2, 3}, 0) == -1
+    print("Coding complete? Click 'Check' to earn cool rewards!")
---
+++ 04-end-zeros.py
@@ -0,0 +1,28 @@
+# def end_zeros(num: int) -> int:
+#     numero = str(num)
+#     ceros = 0
+#     i = len(numero)-1
+#     while i >= 0 and numero[i] == "0":
+#         ceros +=1
+#         i -=1
+#     return ceros
+
+def end_zeros(num: int, contador = 0) -> int:
+    if num == 0: 
+        return contador + 1
+    if num % 10 == 0:
+        return  end_zeros(num / 10, contador+1)        
+    return contador
+
+if __name__ == '__main__':
+    print("Example:")
+    print(end_zeros(0))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert end_zeros(0) == 1
+    assert end_zeros(1) == 0
+    assert end_zeros(10) == 1
+    assert end_zeros(101) == 0
+    assert end_zeros(245) == 0
+    assert end_zeros(100100) == 2
+    print("Coding complete? Click 'Check' to earn cool rewards!")
