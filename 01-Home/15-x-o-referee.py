72099938a87421f1b305cc1c5812c5e892cb8b27
= <=>
2021-11-20 20:30:20 -0300

    Tateti

--- 01-Home/15-x-o-referee.py
+++ 01-Home/15-x-o-referee.py
@@ -1,28 +1,22 @@
 from typing import List
 
-
 def checkio(game_result: List[str]) -> str:
-
     for i in range(3):
-        if sum(game_result[i]) == 3:
-            return "X"
-        elif sum(game_result[i]) == 30:
-            return "0"
-        if sum(game_result[i][j] for j in range(3)) == 3:
-            return "X"
-        elif sum(game_result[i][j] for j in range(3)) == 30:
-            return "0"
-    
-    if game_result[0][0] + game_result[1][1] + game_result[2][2] == 3:
-        return "X"
-    if game_result[0][0] + game_result[1][1] + game_result[2][2] == 30:
-        return "0"
-    if game_result[0][2] + game_result[1][1] + game_result[0][2] == 3:
-        return "X"
-    if game_result[0][2] + game_result[1][1] + game_result[0][2] == 30:
-        return "0"
+        if len(set(game_result[i])) == 1 and game_result[i][0] != ".":
+            return game_result[i][0]
+
+        if len(set([game_result[0][i],game_result[1][i],game_result[2][i]]  )) == 1 and game_result[0][i] != ".":
+            return game_result[0][i]
+
+    if len(set([game_result[0][0],game_result[1][1],game_result[2][2]]  )) == 1 and game_result[0][0] != ".":
+        return game_result[0][0]
+    if len(set([game_result[0][2],game_result[1][1],game_result[2][0]]  )) == 1 and game_result[0][2] != ".":
+        return game_result[0][2]
 
     return "D"
+
+
+
 
 
 if __name__ == "__main__":
@@ -30,6 +24,7 @@ if __name__ == "__main__":
     print(checkio(["X.O", "XX.", "XOO"]))
 
     # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(["...","XXX","OO."]) == "X", "X wins"
     assert checkio(["X.O", "XX.", "XOO"]) == "X", "X wins"
     assert checkio(["OO.", "XOX", "XOX"]) == "O", "O wins"
     assert checkio(["OOX", "XXO", "OXX"]) == "D", "Draw"
